2999b50dffd7dd167ad244c2d236324fb55304ae
Paul Carr <pwcarr15@gmail.com>
2021-05-10 17:35:37 -0400

    Fixed application error.

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 // Routes for the api notes page to get the notes, push the notes to be stored, and delete the notes
 const fs = require("fs");
-let constant = JSON.parse(fs.readFileSync("./db.json", "utf8"));
 const path = require("path");
 
 module.exports = function(app) {
