da8508cea976c7a8862408e2cbd0340943c350b4
Paul Carr <pwcarr15@gmail.com>
2021-05-10 17:20:32 -0400

    Restructure and more backend work.

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// Lets it write files and run express
 const express = require("express");
+const fs = require("fs");
+
+// Lets express run and sets where the port is
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.json(path.join(__dirname, "index.html"))
-});
+// Middleware
+app.use(express.json());
+app.use("/assets", express.static("assets"));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log("you've connected at http://localhost:" + PORT);
+// Lets the port listen for commands
+app.listen(PORT, function() {
+    console.log("App is listening on PORT " + PORT);
 });
