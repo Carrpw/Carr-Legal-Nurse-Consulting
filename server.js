e9b756f963771ce0951463152f8a6e2def91cb70
Paul Carr <pwcarr15@gmail.com>
2021-05-11 21:41:57 -0400

    comment 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Lets it write files and run express
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 // Lets express run and sets where the port is
 const app = express();
@@ -9,11 +10,14 @@ const PORT = process.env.PORT || 3000
 // Middleware
 app.use(express.json());
 // app.use("/assets", express.static("assets"));
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes for the api and htmls
-require("./routes")(app);
+// require("./routes")(app);
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 // Lets the port listen for commands
 app.listen(PORT, function() {
